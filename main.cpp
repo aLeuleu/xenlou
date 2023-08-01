72f8f4475b37382aea4fd183d31e2bcc9354aef8
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2023-08-02 01:20:55 +0200

    not_working

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 
 #include <SDL.h>
 
@@ -7,116 +7,21 @@
 #define SCREEN_HEIGHT   600
 SDL_Rect create_rect();
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	// Unused argc, argv
 	(void) argc;
 	(void) argv;
 
-	// Initialize SDL
-	if(SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		std::cout << "SDL could not be initialized!" << std::endl
-				  << "SDL_Error: " << SDL_GetError() << std::endl;
-		return 0;
-	}
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window* window = SDL_CreateWindow("xenlou",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,
+		SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-#if defined linux && SDL_VERSION_ATLEAST(2, 0, 8)
-	// Disable compositor bypass
-    if(!SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0"))
-    {
-        std::cout << "SDL can not disable compositor bypass!" << std::endl;
-        return 0;
-    }
-#endif
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(3000);
 
-	// Create window
-	SDL_Window *window = SDL_CreateWindow("Basic C++ SDL project",
-										  SDL_WINDOWPOS_UNDEFINED,
-										  SDL_WINDOWPOS_UNDEFINED,
-										  SCREEN_WIDTH, SCREEN_HEIGHT,
-										  SDL_WINDOW_SHOWN);
-
-	if(!window)
-	{
-		std::cout << "Window could not be created!" << std::endl
-				  << "SDL_Error: " << SDL_GetError() << std::endl;
-		// Quit SDL
-		SDL_Quit();
-		exit(EXIT_FAILURE);
-	}
-
-
-	// Create renderer
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	if(!renderer)
-	{
-		std::cout << "Renderer could not be created!" << std::endl
-				  << "SDL_Error: " << SDL_GetError() << std::endl;
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		exit(EXIT_FAILURE);
-	}
-
-	SDL_Rect squareRect = create_rect();
-
-
-	// Event loop exit flag
-	bool quit = false;
-
-	// Event loop
-	while(!quit)
-	{
-		SDL_Event e;
-
-		// Wait indefinitely for the next available event
-		SDL_WaitEvent(&e);
-
-		// User requests quit
-		if(e.type == SDL_QUIT)
-		{
-			quit = true;
-		}
-
-		// Initialize renderer color white for the background
-		SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-		// Clear screen
-		SDL_RenderClear(renderer);
-
-		// Set renderer color red to draw the square
-		SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-
-		// Draw filled square
-		SDL_RenderFillRect(renderer, &squareRect);
-
-		// Update screen
-		SDL_RenderPresent(renderer);
-	}
-
-	// Destroy renderer
-	SDL_DestroyRenderer(renderer);
-
-	// Destroy window
-	SDL_DestroyWindow(window);
-
-
-	// Quit SDL
-	SDL_Quit();
-
-	return 0;
-}
-
-
-SDL_Rect create_rect() {
-	SDL_Rect squareRect;
-
-	// Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-	squareRect.w = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-	squareRect.h = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-
-	// Square position: In the middle of the screen
-	squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-	squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
-	return squareRect;
+	return (0);
 }
